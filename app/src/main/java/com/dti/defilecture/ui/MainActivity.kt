2dee8176ae67ec342f3a2b63fdb15df05ed8a57e
snely-coder <snely550@gmail.com>
2022-10-26 17:01:48 -0400

    Test premier d'un premier commit.

---
+++ app/src/main/java/com/dti/defilecture/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dti.defilecture.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dti.defilecture.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
