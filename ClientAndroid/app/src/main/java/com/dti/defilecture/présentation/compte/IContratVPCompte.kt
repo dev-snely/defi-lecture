12cb0df554b1c0a64e777c6c415ebdbdd4b198fa
vvule <1682121@crosemont.qc.ca>
2022-11-23 19:03:08 -0500

    Ajout et développement de la page compte_utilisateur et modification de façon MVP.
    Manque le développement pour la récupération des informations dans la page compte_utilisateur et modification.
    
    Pour la vue_coin_rond.xml - Exemple tiré de: https://www.geeksforgeeks.org/how-to-add-a-textview-with-rounded-corner-in-android/

---
+++ ClientAndroid/app/src/main/java/com/dti/defilecture/présentation/compte/IContratVPCompte.kt
@@ -0,0 +1,19 @@
+package com.dti.defilecture.présentation.compte
+
+import com.dti.defilecture.R
+
+
+interface IContratVPCompte {
+
+
+    interface IVueCompte{
+        fun naviguerVersModifier()
+    }
+
+    interface IPrésentateurCompte{
+
+
+    }
+
+
+}
