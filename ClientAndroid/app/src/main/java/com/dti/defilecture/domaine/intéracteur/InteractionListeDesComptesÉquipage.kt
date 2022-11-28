9fab7da3838b5d51900320558edfbbb3bc94ac29
Gerard Minh Dat Truong <gtruong@dept-info.crosemont.quebec>
2022-11-28 14:56:34 -0500

    Ajout Intéraction avec "SourceDeDonnéesÉquipageHTTP" en commentaire (Attente du URL)

--- ClientAndroid/app/src/main/java/com/dti/defilecture/domaine/intéracteur/InteractionListeDesComptesÉquipage.kt
+++ ClientAndroid/app/src/main/java/com/dti/defilecture/domaine/intéracteur/InteractionListeDesComptesÉquipage.kt
@@ -2,15 +2,16 @@ package com.dti.defilecture.domaine.intéracteur
 
 import com.dti.defilecture.accesAuxDonnees.ISourcesDeDonnéesÉquipage
 import com.dti.defilecture.accesAuxDonnees.SourceDeComptesBidons
+import com.dti.defilecture.accesAuxDonnees.SourceDeDonnéesÉquipageHTTP
 import com.dti.defilecture.domaine.entité.Compte
 
-class InteractionListeDesComptesÉquipage(  ) {
+class InteractionListeDesComptesÉquipage() {
 
 
-    var sourceDeDonnées: ISourcesDeDonnéesÉquipage = SourceDeComptesBidons()
+    var sourceDeDonnées: ISourcesDeDonnéesÉquipage = SourceDeComptesBidons() //SourceDeDonnéesÉquipageHTTP(context, URL)
 
-    var compte: Compte = Compte("","",0, "", "", "", "")
-
+    var compte: Compte = Compte("","",0, "",
+        "", "", "")
 
     fun initialiser(): Compte{
         return compte
