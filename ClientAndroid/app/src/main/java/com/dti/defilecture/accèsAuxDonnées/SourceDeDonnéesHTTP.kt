de87f97e830d6f04c533277f261d58443dc4b916
SLys <1826841@crosemont.qc.ca>
2022-12-04 02:39:12 -0500

    Ajout de la désérialisation dans la source de données HTTP.

--- ClientAndroid/app/src/main/java/com/dti/defilecture/accèsAuxDonnées/SourceDeDonnéesHTTP.kt
+++ ClientAndroid/app/src/main/java/com/dti/defilecture/accèsAuxDonnées/SourceDeDonnéesHTTP.kt
@@ -1,13 +1,38 @@
 package com.dti.defilecture.accèsAuxDonnées
 
+import android.content.Context
+import android.util.JsonReader
+import com.android.volley.Request
+import com.android.volley.toolbox.RequestFuture
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.dti.defilecture.domaine.entité.Compte
 import com.dti.defilecture.domaine.entité.Lecture
 import com.dti.defilecture.domaine.entité.Questionnaire
 import com.dti.defilecture.domaine.entité.Équipage
+import java.io.StringReader
+import java.net.URL
+import java.util.concurrent.ExecutionException
 
-class SourceDeDonnéesHTTP : ISourceDeDonnées {
+class SourceDeDonnéesHTTP(var ctx: Context, var urlSource: URL) : ISourceDeDonnées {
     override fun validerComptePourLaConnexion(pseudo: String, mdp: String): Compte? {
-        TODO("Not yet implemented")
+        val queue = Volley.newRequestQueue(ctx)
+
+        val promesse: RequestFuture<String> = RequestFuture.newFuture()
+
+        val requête = StringRequest(Request.Method.GET,
+            "$urlSource/api/Compte/$pseudo/$mdp", promesse, promesse)
+        queue.add(requête);
+
+        try {
+            return réponseJsonToCompte( JsonReader( StringReader( promesse.get() )) )
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+            return Compte()
+        }catch (e: ExecutionException) {
+            throw AccèsRessourcesException( e )
+        }
+
     }
 
     override fun obtenirListeDesComptesÉquipage(): MutableList<Compte> {
@@ -29,4 +54,63 @@ class SourceDeDonnéesHTTP : ISourceDeDonnées {
     override fun obtenirListeDesÉquipages(): MutableList<Équipage>? {
         TODO("Not yet implemented")
     }
+
+    /**
+     * Méthode qui convertit la réponse JSON en objet kotlin Compte.
+     *
+     * @param jsonReader Objet permettant la lecture d'un objet JSON
+     */
+    private fun réponseJsonToCompte(jsonReader: JsonReader): Compte {
+        val compte = Compte()
+
+        jsonReader.beginObject()
+
+        while (jsonReader.hasNext()) {
+
+            val clé = jsonReader.nextName()
+
+            when (clé) {
+                "idCompte" -> {
+                    compte.idCompte = jsonReader.nextInt()
+                }
+                "courriel" -> {
+                    compte.courriel = jsonReader.nextString()
+                }
+                "motDePasse" -> {
+                    compte.motDePasse = jsonReader.nextString()
+                }
+                "nom" -> {
+                    compte.nom = jsonReader.nextString()
+                }
+                "prenom" -> {
+                    compte.prenom = jsonReader.nextString()
+                }
+                "pseudo" -> {
+                    compte.pseudonyme = jsonReader.nextString()
+                }
+                "role" -> {
+                    compte.role = jsonReader.nextInt()
+                }
+                "avatar" -> {
+                    compte.avatar = jsonReader.nextString()
+                }
+                "point" -> {
+                    compte.doublons = jsonReader.nextInt()
+                }
+                "programme" -> {
+                    compte.programme = jsonReader.nextString()
+                }
+                "devenirCapitaine" -> {
+                    compte.devenirCapitaine = jsonReader.nextBoolean()
+                }
+                else -> {
+                    jsonReader.skipValue()
+                }
+            }
+        }
+        jsonReader.endObject()
+
+        return compte
+    }
+
 }
